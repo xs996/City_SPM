ea5ae0b3f2ca4fc529ea7e933789b75b2db940cb
xieshuai <xieshuai96@foxmail.com>
2019-09-20 11:29:48 +0800

    MHP-101 市平台SPA模式框架研究与调整: >>新增项目文件\City_SPM\Code\city\project

---
+++ Code/city/project/src/main/java/com/hyd/HydApplication.java
@@ -0,0 +1,13 @@
+package com.hyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydApplication.class, args);
+    }
+
+}
