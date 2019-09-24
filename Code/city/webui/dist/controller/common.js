9e9084fe33c9a6516c66010cf8a4b1c32d6f31a2
denglinkun <123123>
2019-09-24 14:07:24 +0800

    提交登陆页面

--- Code/city/webui/dist/controller/common.js
+++ Code/city/webui/dist/controller/common.js
@@ -26,16 +26,20 @@ layui.define(function(exports){
             btn: ['确认', '取消'],
             yes: function(index, layero) {
                 //执行退出接口
-                admin.req({
-                    url: '/rest/SysCtr/logout'
-                    ,type: 'post'
-                    ,done: function(res){ //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
-                        layer.close(index);
-                        //清空本地记录的 token，并跳转到登入页
-                        admin.exit();
-                        debugger;
-                    }
-                });
+                // admin.req({
+                //     url: '/rest/SysCtr/logout'
+                //     ,type: 'post'
+                //     ,done: function(res){ //这里要说明一下：done 是只有 response 的 code 正常才会执行。而 succese 则是只要 http 为 200 就会执行
+                //         layer.close(index);
+                //         //清空本地记录的 token，并跳转到登入页
+                //         admin.exit();
+                //         debugger;
+                //     }
+                // });
+
+                admin.exit();
+                window.location='../login/login.html';
+                debugger;
             }
         });
 
